ab54a56ae58fa89930059af3969a78fbbb6d71b8
MoonSam <ty5623@naver.com>
2022-11-23 12:09:11 +0900

    add install shell

---
+++ install_debian.sh
@@ -0,0 +1,7 @@
+sudo apt install build-essential devscripts debhelper check libsubunit-dev fakeroot pkg-config dkms 
+cd packages 
+#CUDA=<cuda-install-top-dir> ./build-deb-packages.sh 
+#sudo dpkg -i gdrdrv-dkms_<version>_<arch>.<platform>.deb 
+#sudo dpkg -i libgdrapi_<version>_<arch>.<platform>.deb 
+#sudo dpkg -i gdrcopy-tests_<version>_<arch>.<platform>.deb 
+#sudo dpkg -i gdrcopy_<version>_<arch>.<platform>.deb 
